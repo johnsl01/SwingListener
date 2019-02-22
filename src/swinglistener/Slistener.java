7d18d291beae79b8c683e208118f21b563d44992
johnsl01 <14140299+johnsl01@users.noreply.github.com>
2019-02-22 17:59:49 +0000

    Update Slistener.java

--- src/swinglistener/Slistener.java
+++ src/swinglistener/Slistener.java
@@ -1,3 +1,21 @@
 /** 
 * Slistener
+*
+* port listener class for Swing Listener 
+*
 */
+
+public class Slistener
+             implements Runnable // because it is started as a thread by the gui
+{
+    // class variables 
+    int port ; // the port on which to listen
+    
+  
+    public Slistener ( int port)
+    {
+        this.port = port; // pick up port parameter and pass to class variable
+        
+  
+  
+} // class Slistener
